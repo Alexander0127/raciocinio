a5fc3d7e22f40867762b57a5059248230562dfc5
Alexander0127 <53129255+Alexander0127@users.noreply.github.com>
2022-11-29 13:41:54 -0500

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,35 @@
+const $paso_1 = document.getElementById("paso-1");
+const $clase_paso_1 = document.querySelector(".contenedor-paso-1");
+$paso_1.onclick = function (e) {
+    $clase_paso_1.classList.toggle("visible");
+};
+const $paso_2 = document.getElementById("paso-2");
+const $clase_paso_2 = document.querySelector(".contenedor-paso-2");
+$paso_2.onclick = function (e) {
+    $clase_paso_2.classList.toggle("visible");
+};
+const $paso_3 = document.getElementById("paso-3");
+const $clase_paso_3 = document.querySelector(".contenedor-paso-3");
+$paso_3.onclick = function (e) {
+    $clase_paso_3.classList.toggle("visible");
+};
+const $paso_4 = document.getElementById("paso-4");
+const $clase_paso_4 = document.querySelector(".contenedor-paso-4");
+$paso_4.onclick = function (e) {
+    $clase_paso_4.classList.toggle("visible");
+};
+const $paso_5 = document.getElementById("paso-5");
+const $clase_paso_5 = document.querySelector(".contenedor-paso-5");
+$paso_5.onclick = function (e) {
+    $clase_paso_5.classList.toggle("visible");
+};
+const $paso_6 = document.getElementById("paso-6");
+const $clase_paso_6 = document.querySelector(".contenedor-paso-6");
+$paso_6.onclick = function (e) {
+    $clase_paso_6.classList.toggle("visible");
+};
+const $paso_7 = document.getElementById("paso-7");
+const $clase_paso_7 = document.querySelector(".contenedor-paso-7");
+$paso_7.onclick = function (e) {
+    $clase_paso_7.classList.toggle("visible");
+};
